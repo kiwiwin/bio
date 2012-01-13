898d50ae93ffa1a1643d36e1888e2b5c6a398444
kiwi.win <547880119@qq.com>
2012-01-14 00:31:56 +0800

    add a simple products service

---
+++ bio.Web.Test/ProductTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bio.Web.Test
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void TestGetXmlString()
+        {
+            XElement xml = new XElement("Product",
+                new XElement("Id", "0101-100KG"),
+                new XElement("EnglishName", "CITRIC ACID, TRISODIUM SALT, DIHYDRATE"),
+                new XElement("ChineseName", "柠檬酸三钠,二水"),
+                new XElement("Price", "13500")); 
+
+            Product product = ProductTable.Instance.GetProductById("0101-100KG");
+            Assert.AreEqual(xml.ToString(), ProductTable.Instance.GetProductXmlString(product));            
+        }
+
+        [TestMethod]
+        public void TestGetXmlElement()
+        {
+            XElement xml = new XElement("Product",
+                new XElement("Id", "0101-100KG"),
+                new XElement("EnglishName", "CITRIC ACID, TRISODIUM SALT, DIHYDRATE"),
+                new XElement("ChineseName", "柠檬酸三钠,二水"),
+                new XElement("Price", "13500"));
+
+            Product product = ProductTable.Instance.GetProductById("0101-100KG");
+            Assert.AreEqual(xml.ToString(), product.GetXmlElement().ToString());
+        }
+    }
+}
--- bio/MainPage.xaml.cs
+++ bio/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
+using bio.ProductService;
+using System.Xml.Linq;
+
 namespace bio
 {
     public partial class MainPage : UserControl
@@ -17,6 +21,32 @@ namespace bio
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void buttonGetAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            ProductServiceSoapClient client = new ProductServiceSoapClient();
+            client.GetAllProductsCompleted += GetAllProductsCompleted;
+            client.GetAllProductsAsync();
+        }
+
+        private void GetAllProductsCompleted(object sender, GetAllProductsCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Error == null)
+                {
+                    IList<Product> products = new List<Product>();
+                    foreach (var product in e.Result)
+                    {
+                        products.Add(product);
+                    }
+                    dataGridProducts.ItemsSource = products;
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
