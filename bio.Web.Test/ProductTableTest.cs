75a3818af199241aa8b79fa09579983e68f5c92c
kiwi.win <547880119@qq.com>
2012-01-13 15:04:07 +0800

    add test excel configuration

--- bio.Web.Test/ProductTableTest.cs
+++ bio.Web.Test/ProductTableTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Configuration;
 using System.Data.Common;
 using System.Data.OleDb;
 using System.Text;
@@ -15,22 +16,21 @@ namespace bio.Web.Test
         [TestMethod]
         public void TestGetAllProdcuts()
         {
-            ProductTable productsTable = new ProductTable(@"Provider=Microsoft.ACE.OleDb.12.0;Data Source=C:\Users\Administrator\bio\bio.Web.Test\testProduct.xlsx;Extended Properties='Excel 12.0;HDR=YES'");
+            ProductTable productsTable = new ProductTable(ConfigurationManager.ConnectionStrings["ProductTableTest"].ConnectionString);
             IEnumerable<Product> products = productsTable.GetAllProducts();
+            Assert.IsNotNull(products);
             Assert.AreEqual(4, products.Count());
         }
 
         [TestMethod]
         public void TestGetProductById()
         {
-            ProductTable productsTable = new ProductTable(@"Provider=Microsoft.ACE.OleDb.12.0;Data Source=C:\Users\Administrator\bio\bio.Web.Test\testProduct.xlsx;Extended Properties='Excel 12.0;HDR=YES'");
+            ProductTable productsTable = new ProductTable(ConfigurationManager.ConnectionStrings["ProductTableTest"].ConnectionString);
             Product product = productsTable.GetProductById("0101-100KG");
-            //Product product = productsTable.GetAllProducts().First();
             Assert.IsNotNull(product);
             Assert.AreEqual("0101-100KG", product.Id);
             Assert.AreEqual("CITRIC ACID, TRISODIUM SALT, DIHYDRATE", product.EnglishName);
             Assert.AreEqual("柠檬酸三钠,二水", product.ChineseName);
-            //Assert.AreEqual(13500, product.Price, 0.1);
             Assert.AreEqual("13500", product.Price);
         }
     }
